8d86c427380fca6125d868084fd7ddfa089d02d9
Vinicyos Ferreira <vinifr987@gmail.com>
2026-01-14 15:31:10 -0300

    feat: add api interceptors to ensure that tokens use headers authorization

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,21 @@
 import axios from 'axios';
 
+import { ACCESS_TOKEN_KEY } from '@/constant/local-storage';
+
 const api = axios.create({
   baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com',
+});
+
+api.interceptors.request.use((request) => {
+  // buscar o token no localstorage
+  const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
+
+  // verificar se existe o access token
+  if (!accessToken) return request;
+
+  // adicionar as requisições da aplicação o token com autorização
+  request.headers.Authorization = `Bearer ${accessToken}`;
+  return request;
 });
 
 export default api;
