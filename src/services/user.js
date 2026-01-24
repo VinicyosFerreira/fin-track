6e64dc98dc10a913706334977ab0c07c03cf992b
Vinicyos Ferreira <vinifr987@gmail.com>
2026-01-23 22:00:31 -0300

    docs: add JsDocs to UserService

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,16 @@
 import { protectedApi, publicApi } from '@/lib/axios';
 
 const UserService = {
+  /** 
+     Create a new user account
+     * @param {Object} variables User the signup details
+     * @param {string} variables.firstName the user's first name
+     * @param {string} variables.lastName  the user's last name
+     * @param {string} variables.email the user's email address
+     * @param {string} variables.password the user's password
+     * @returns {Promise<Object>} The created a user
+     * @returns {Promise<string>} response.tokens the user's tokens
+  */
   signup: async (variables) => {
     const response = await publicApi.post('/api/users', {
       first_name: variables.firstName,
@@ -9,7 +19,6 @@ const UserService = {
       password: variables.password,
     });
 
-    // retornar o padrão de resposta
     return {
       id: response.data.id,
       firstName: response.data.first_name,
@@ -18,6 +27,15 @@ const UserService = {
       tokens: response.data.tokens,
     };
   },
+
+  /**
+      Authenticate user and retrieve tokens
+      * @param {Object} variables  User login details
+      * @param {string} variables.email  the user's email address
+      * @param {string} variables.password  the user's password
+      * @returns {Promise<Object>} The logged in user 
+      * @returns {Promise<string>} response.tokens the user's tokens
+   */
   login: async (variables) => {
     const response = await publicApi.post('api/users/login', {
       email: variables.email,
@@ -32,6 +50,10 @@ const UserService = {
     };
   },
 
+  /**
+        Retrieve tokens refresh and access tokens
+      * @returns {Promise<Object>} The user authenticated
+   */
   me: async () => {
     const response = await protectedApi.get('api/users/me');
     return {
