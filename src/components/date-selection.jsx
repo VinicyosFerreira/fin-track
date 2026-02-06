2f5d547fc1f7794d80b9e1c167198e68cd993519
Vinicyos Ferreira <vinifr987@gmail.com>
2026-02-06 16:05:20 -0300

    feat: ensure selected date in persisted on page reload

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -1,13 +1,36 @@
 import { addMonths } from 'date-fns';
-import { useState } from 'react';
+import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router';
 
 import { DatePickerWithRange } from './ui/date-picker-with-range';
 
 const DataSelection = () => {
+  const [searchParams] = useSearchParams();
   const [date, setDate] = useState({
-    from: new Date(),
-    to: addMonths(new Date(), 1),
+    from: searchParams.get('from')
+      ? new Date(searchParams.get('from') + 'T00:00:00')
+      : new Date(),
+    to: searchParams.get('to')
+      ? new Date(searchParams.get('to') + 'T00:00:00')
+      : addMonths(new Date(), 1),
   });
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!date?.from || !date?.to) return;
+    // adicionar os parâmetros da URL
+    const queryParams = new URLSearchParams();
+    queryParams.set('from', format(date.from, 'yyyy-MM-dd'));
+    queryParams.set('to', format(date.to, 'yyyy-MM-dd'));
+
+    // salvar as informações na URL do navegador via navigate
+    navigate({
+      search: queryParams.toString(),
+    });
+  }, [date, navigate]);
+
   return (
     <div>
       <DatePickerWithRange
