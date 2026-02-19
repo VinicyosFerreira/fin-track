8c55d226c27f7afb575e2b25593e88178e099530
Vinicyos Ferreira <vinifr987@gmail.com>
2026-02-19 13:57:51 -0300

    feat: add 'getAll' to the API service to fetch transactions

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -1,8 +1,21 @@
+import queryString from 'query-string';
+
 import { protectedApi } from '@/lib/axios';
 
 const TransactionService = {
   create: async (variables) => {
     const response = await protectedApi.post('/api/transactions/me', variables);
+    return response.data;
+  },
+  getAll: async (variables) => {
+    const queryParams = queryString.stringify({
+      from: variables.from,
+      to: variables.to,
+    });
+
+    const response = await protectedApi.get(
+      `/api/transactions/me?${queryParams}`
+    );
     return response.data;
   },
 };
